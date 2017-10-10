9b9bc58297507d548235628c60fe637831ba2b9c
Julio Olivera <julio.olvr@gmail.com>
2017-10-09 23:40:37 -0700

    search fn in lib

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,7 @@ fn main() {
                                                        process::exit(1);
                                                    });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
-
+    if let Err(_) = minigrep::run(config) {
         process::exit(1);
     }
 }

19f2b347ba8beb59259ec904e84b9ae660151728
Julio Olivera <julio.olvr@gmail.com>
2017-10-09 23:55:33 -0700

    Print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-                                                       println!("Problem parsing arguments: {}",
-                                                                err);
+                                                       eprintln!("Problem parsing arguments: {}",
+                                                                 err);
                                                        process::exit(1);
                                                    });
 
-    if let Err(_) = minigrep::run(config) {
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
